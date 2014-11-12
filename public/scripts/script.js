18e6feb38721bcaaf58ee8cd670dc2ad365f3593
cedmax <cedmax@users.noreply.github.com>
2014-11-12 23:51:55 +0000

    better ui

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,11 @@
 /*global Handlebars, FileLoader, console, progressBar,createPlaylist, findSongs */
+
+function reset() {
+	'use strict';
+	$( '#end' ).remove();
+	$( '#progressBarContainer' ).hide();
+}
+
 ( function() {
 	'use strict';
 	/**
@@ -18,6 +25,28 @@
 	function getWeek() {
 		var onejan = new Date( ( new Date() ).getFullYear(), 0, 1 );
 		return Math.ceil( ( ( ( new Date() - onejan ) / 86400000 ) + onejan.getDay() + 1 ) / 7 );
+	}
+
+	function showLoggedIn() {
+		$( '#loggedin' ).show();
+		$( '#shazam' ).animate( {
+			left: '-100%'
+		}, 3000 );
+		$( '#spotify' ).animate( {
+			right: '-100%'
+		}, 3000 );
+		$( '#login' ).animate( {
+			opacity: '0'
+		}, 3000, function() {
+			$( '#login' ).remove();
+		} );
+	}
+
+	function showPlaylist( uri ) {
+		$( '#progressBar' ).hide();
+		$( '#progressBar' ).after(
+			'<div id="end" class="dialog"><a class="btn btn-primary" onclick="window.open(\'' + uri + '\');return false;">See your playlist</a><br/><br/><span>or</span><br/><br/><a class="btn btn-primary" onclick="reset();">Create a new one</a></div'
+		);
 	}
 
 	var userProfileSource = document.getElementById( 'user-profile-template' ).innerHTML,
@@ -55,14 +84,11 @@
 										uri: $( '#countries' ).val()
 									} ),
 									success: function( songs ) {
-										findSongs( songs, userResponse.country, function( spotifySongs ) {
+										findSongs( songs, userResponse.country, progressBar, function( spotifySongs ) {
 											createPlaylist( spotifySongs, $( '#countries option:selected' ).text() + ' Chart - week #' + getWeek(), {
 												accessToken: access_token,
 												userId: userResponse.id,
-												success: function( uri ) {
-													$( '#submit' ).after( $( '<a onclick="window.open(\'' + uri + '\');return false;">DONE!</a>' ) );
-													$( '#submit' ).remove();
-												}
+												success: showPlaylist
 											} );
 										} );
 									}
@@ -71,25 +97,18 @@
 						}
 					} );
 
-					new FileLoader( userResponse.country, progressBar, function( songs ) {
+					new FileLoader( userResponse.country, findSongs, progressBar, function( songs ) {
 						createPlaylist( songs, 'MyShazam', {
 							accessToken: access_token,
 							reverse: true,
 							userId: userResponse.id,
-							success: function( uri ) {
-								document.getElementById( 'viewport_bk_img' ).style.display = 'none';
-								document.getElementById( 'viewport_bk' ).innerHTML = '<a onclick="window.open(\'' + uri + '\');return false;">DONE!</a>';
-							}
+							success: showPlaylist
 						} );
 					} );
 
-					$( '#login' ).hide();
-					$( '#loggedin' ).show();
+					showLoggedIn();
 				}
 			} );
-		} else {
-			$( '#login' ).show();
-			$( '#loggedin' ).hide();
 		}
 	}
 } )();
