81382c26fd546628e5f77da1b002dcb92d79b6aa
cedmax <marco.cedaro@gmail.com>
2014-11-25 21:20:43 +0000

    moved youtube find to server

---
+++ controller/youtubeSearch.js
@@ -0,0 +1,27 @@
+module.exports = function() {
+	'use strict';
+
+	var querystring = require( 'querystring' );
+	var google = require( 'googleapis' );
+	var youtube = google.youtube( 'v3' );
+	var client = require( '../settings' ).youtube;
+
+	return function( req, res ) {
+
+		var searchOptions = {
+			q: req.query.q,
+			part: 'snippet',
+			maxResults: 1,
+			type: 'video',
+			videoCategoryId: 10,
+			auth: client.apiKey
+		}
+
+		youtube.search.list( searchOptions, function( error, response ) {
+			if ( !error && response ) {
+				res.json( response );
+			}
+		} );
+
+	};
+};
--- public/scripts/youtube/createPlaylist.js
+++ public/scripts/youtube/createPlaylist.js
@@ -1,51 +1,51 @@
 function createPlaylist( songs, title, config ) {
 	'use strict';
 
-		gapi.client.load('youtube', 'v3', function() {
-			function fillPlaylist(playlistId, queue){
-				var song = queue.shift();
-				progressBar.update();
+	gapi.client.load( 'youtube', 'v3', function() {
+		function fillPlaylist( playlistId, queue ) {
+			var song = queue.shift();
+			progressBar.update();
 
-				if (song){
-					var request = gapi.client.youtube.playlistItems.insert({
-						part: 'snippet',
-						resource: {
-							snippet: {
-								playlistId: playlistId,
-								resourceId: {
-									videoId: song,
-									kind: 'youtube#video'
-								}
+			if ( song ) {
+				var request = gapi.client.youtube.playlistItems.insert( {
+					part: 'snippet',
+					resource: {
+						snippet: {
+							playlistId: playlistId,
+							resourceId: {
+								videoId: song,
+								kind: 'youtube#video'
 							}
 						}
-					});
-					request.execute(function(response){
-						fillPlaylist(playlistId, queue);
-					});
-				} else {
-					progressBar.finish();
-					config.success( playlistId );
+					}
+				} );
+				request.execute( function( response ) {
+					fillPlaylist( playlistId, queue );
+				} );
+			} else {
+				progressBar.finish();
+				config.success( playlistId );
+			}
+		}
+
+		var request = gapi.client.youtube.playlists.insert( {
+			part: 'snippet,status',
+			resource: {
+				snippet: {
+					title: title
+				},
+				status: {
+					privacyStatus: 'private'
 				}
 			}
-
-			var request = gapi.client.youtube.playlists.insert({
-				part: 'snippet,status',
-				resource: {
-					snippet: {
-						title: title
-					},
-					status: {
-						privacyStatus: 'private'
-					}
-				}
-			});
-			request.execute(function(response) {
-				var result = response.result;
-				if (result) {
-					var playlistId = result.id;
-					progressBar = progressBar( songs.length );
-					fillPlaylist(playlistId, songs);
-				}
-			});
-		});
+		} );
+		request.execute( function( response ) {
+			var result = response.result;
+			if ( result ) {
+				var playlistId = result.id;
+				progressBar = progressBar( songs.length );
+				fillPlaylist( playlistId, songs );
+			}
+		} );
+	} );
 }
