810aeb6e3a146b5f8c691f0c066d9848823c2d13
Marco Cedaro <cedmax@users.noreply.github.com>
2015-07-11 16:25:05 +0100

    fix charts, adding new ones too

--- assets/scripts/fetchCharts.js
+++ assets/scripts/fetchCharts.js
@@ -2,26 +2,40 @@ function fetchCharts( market, config ) {
 	$.ajax( {
 		url: '/charts',
 		success: function( chartResponse ) {
-			for ( var nation in chartResponse ) {
-				$( '#countries' ).append( '<option value="' + chartResponse[ nation ] + '">' + nation + '</option>' );
-			}
+			$('#chart span').hide();
+			for ( var key in chartResponse ) {
+				if (chartResponse.hasOwnProperty(key)){
+					var id = key.replace(/ +?/g, '');
+					
+					$('#chart').append(
+						$.handlebarTemplates.charts({
+							title: key,
+							id: id
+						})
+					);
 
-			$( '#submit' ).on( 'click', function() {
-				$( '#submit' ).attr( 'disabled', 'disabled' );
-				$.ajax( {
-					url: '/charts',
-					type: 'POST',
-					contentType: 'application/json',
-					data: JSON.stringify( {
-						uri: $( '#countries' ).val()
-					} ),
-					success: function( songs ) {
-						findSongs( songs, market, progressBar, function( songResult ) {
-							createPlaylist( songResult, $( '#countries option:selected' ).text() + ' Chart - week #' + getWeek(), config );
-						} );
+					for ( var nation in chartResponse[key] ) {
+						$( '#' + id ).append( '<option value="' + chartResponse[key][ nation ] + '">' + nation + '</option>' );
 					}
-				} );
-			} );
+
+					$( '#submit-' + id ).on( 'click', function() {
+						$( '#submit' + id ).attr( 'disabled', 'disabled' );
+						$.ajax( {
+							url: '/charts',
+							type: 'POST',
+							contentType: 'application/json',
+							data: JSON.stringify( {
+								uri: $( '#' + id ).val()
+							} ),
+							success: function( songs ) {
+								findSongs( songs, market, progressBar, function( songResult ) {
+									createPlaylist( songResult, $( '#'+ id + ' option:selected' ).text() + ' ' + key  +' Chart - week #' + getWeek(), config );
+								} );
+							}
+						} );
+					} );
+				}
+			};
 		}
 	} );
 }
