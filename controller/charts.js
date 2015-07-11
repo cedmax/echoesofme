7a94567f765606a05645199a3a14e2fd50a341f1
cedmax <cedmax@users.noreply.github.com>
2015-07-11 16:25:05 +0100

    fix charts, adding new ones too

--- controller/charts.js
+++ controller/charts.js
@@ -3,20 +3,52 @@ module.exports = function( req, res ) {
 
 	var domain = 'http://www.shazam.com';
 	var cheerio = require( 'cheerio' );
+	var async = require( 'async' );
 	var request = require( 'request' );
+	var map = require('lodash.map');
+	
+	var fetch = function(page, cb){
+		request( domain + page.path, function( err, response, body ) { 
+			if ( err ){
+				cb(err);
+			} else {
+
+				cb(null, {
+					page: page.name,
+					data: body
+				});
+			}
+		});
+	};
 
 	function list() {
 		request( domain + '/charts', function( err, response, body ) {
 			var $ = cheerio.load( body );
 
-			var result = {};
+			var returnObj = {};
 
-			$( '.chrt-nav__select a' ).each( function( i, anchor ) {
-				anchor = $( anchor );
-				result[ anchor.text() ] = anchor.attr( 'href' );
+			var array = map( $( '.chrt-header__links a' ), function( a ){ 
+				return {
+					name: $(a).text(),
+					path: $(a).attr('href')
+				};
 			} );
 
-			res.json( result );
+			async.map(array, fetch, function(err, results){
+				if ( !err ){
+					results.forEach(function(result){
+						var $ = cheerio.load( result.data );
+						var obj = {};
+						$( '.chrt-nav__select a' ).each( function( i, anchor ) {
+							anchor = $( anchor );
+							obj[ anchor.text() ] = anchor.attr( 'href' );
+						} );
+						returnObj[result.page] = obj;
+					});
+				}
+				res.json( returnObj );
+			});
+
 		} );
 	}
 
@@ -27,7 +59,7 @@ module.exports = function( req, res ) {
 			$( '[itemtype="http://schema.org/MusicRecording"]' ).each( function( i, songItem ) {
 				songs.push( {
 					artist: $( songItem ).find( '[itemprop="byArtist"]' ).text().replace( 'by ', '' ).trim(),
-					title: $( songItem ).find( 'h4' ).text()
+					title: $( songItem ).find( '[itemprop="name"]' ).text().trim()
 				} );
 			} );
 			res.json( songs );
