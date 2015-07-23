8358dc04f19d2464b1d3b24f6100745d76708351
Marco Cedaro <cedmax@users.noreply.github.com>
2015-07-23 18:03:25 +0200

    added cache, simplified controllers

--- controller/charts.js
+++ controller/charts.js
@@ -1,77 +1,83 @@
-module.exports = function( req, res ) {
-	'use strict';
+var domain = 'http://www.shazam.com';
+var cheerio = require( 'cheerio' );
+var async = require( 'async' );
+var request = require( 'request' );
+var cachedRequest = require('cached-request')(request);
+cachedRequest.setCacheDirectory("./tmp");
+var map = require('lodash.map');
 
-	var domain = 'http://www.shazam.com';
-	var cheerio = require( 'cheerio' );
-	var async = require( 'async' );
-	var request = require( 'request' );
-	var map = require('lodash.map');
-	
-	var fetch = function(page, cb) {
-		request( domain + page.path, function( err, response, body ) { 
-			if ( err ) {
-				cb(err);
-			} else {
+var fetch = function(page, cb) {
+	cachedRequest( {
+		url: domain + page.path,
+		ttl: 7200000
+	}, function( err, response, body ) { 
+		if ( err ) {
+			cb(err);
+		} else {
 
-				cb(null, {
-					page: page.name,
-					data: body
+			cb(null, {
+				page: page.name,
+				data: body
+			});
+		}
+	});
+};
+
+function list(res) {
+	cachedRequest( {
+		url: domain + '/charts',
+		ttl: 7200000
+	}, function( err, response, body ) {
+		var $ = cheerio.load( body );
+		
+		var returnObj = {};
+
+		var array = map( $( '.chrt-header__links a' ), function( a ) { 
+			return {
+				name: $(a).text(),
+				path: $(a).attr('href')
+			};
+		} );
+
+		async.map(array, fetch, function(err, results) {
+			if ( !err ) {
+				results.forEach(function(result) {
+					var $ = cheerio.load( result.data );
+					var obj = {};
+					$( '.chrt-nav__select a' ).each( function( i, anchor ) {
+						anchor = $( anchor );
+						obj[ anchor.text() ] = anchor.attr( 'href' );
+					} );
+					returnObj[result.page] = obj;
 				});
 			}
+			res.json( returnObj );
 		});
-	};
+	});
+}
 
-	function list() {
-		request( domain + '/charts', function( err, response, body ) {
-			var $ = cheerio.load( body );
-
-			var returnObj = {};
-
-			var array = map( $( '.chrt-header__links a' ), function( a ) { 
-				return {
-					name: $(a).text(),
-					path: $(a).attr('href')
-				};
+function parse(uri, res) {
+	request( domain + uri, function( err, response, body ) {
+		var $ = cheerio.load( body );
+		var songs = [];
+		$( '[itemtype="http://schema.org/MusicRecording"]' ).each( function( i, songItem ) {
+			songs.push( {
+				artist: $( songItem ).find( '[itemprop="byArtist"]' ).text().replace( 'by ', '' ).trim(),
+				title: $( songItem ).find( '[itemprop="name"]' ).text().trim()
 			} );
-
-			async.map(array, fetch, function(err, results) {
-				if ( !err ) {
-					results.forEach(function(result) {
-						var $ = cheerio.load( result.data );
-						var obj = {};
-						$( '.chrt-nav__select a' ).each( function( i, anchor ) {
-							anchor = $( anchor );
-							obj[ anchor.text() ] = anchor.attr( 'href' );
-						} );
-						returnObj[result.page] = obj;
-					});
-				}
-				res.json( returnObj );
-			});
-
 		} );
-	}
+		res.json( songs );
+	} );
+}
 
-	function parse() {
-		request( domain + uri, function( err, response, body ) {
-			var $ = cheerio.load( body );
-			var songs = [];
-			$( '[itemtype="http://schema.org/MusicRecording"]' ).each( function( i, songItem ) {
-				songs.push( {
-					artist: $( songItem ).find( '[itemprop="byArtist"]' ).text().replace( 'by ', '' ).trim(),
-					title: $( songItem ).find( '[itemprop="name"]' ).text().trim()
-				} );
-			} );
-			res.json( songs );
-		} );
-	}
+module.exports = function( req, res ) {
 
 	var uri = req && req.body && req.body.uri;
 
 	if ( uri ) {
-		parse( uri );
+		parse( uri, res );
 	} else {
-		list();
+		list( res );
 	}
 
 };
--- controller/login.js
+++ controller/login.js
@@ -1,32 +1,28 @@
-module.exports = function( client ) {
-	'use strict';
+var querystring = require( 'querystring' );
 
-	var querystring = require( 'querystring' );
+function randomString( length ) {
+	var text = '';
+	var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-	function randomString( length ) {
-		var text = '';
-		var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-		for ( var i = 0; i < length; i ++ ) {
-			text += possible.charAt( Math.floor( Math.random() * possible.length ) );
-		}
-		return text;
+	for ( var i = 0; i < length; i ++ ) {
+		text += possible.charAt( Math.floor( Math.random() * possible.length ) );
 	}
+	return text;
+}
 
-	return function( req, res ) {
-		var stateKey = 'spotify_auth_state';
-		var state = randomString( 16 );
-		res.cookie( stateKey, state );
+module.exports = function( req, res ) {
+	var stateKey = 'spotify_auth_state';
+	var state = randomString( 16 );
+	res.cookie( stateKey, state );
 
-		// your application requests authorization
-		var scope = 'user-read-private user-read-email playlist-read-private playlist-modify-private playlist-modify-public';
-		res.redirect( 'https://accounts.spotify.com/authorize?' +
-			querystring.stringify( {
-				response_type: 'code',
-				client_id: client.spotify.appId,
-				scope: scope,
-				redirect_uri: client.spotify.redirectUri,
-				state: state
-			} ) );
-	};
+	// your application requests authorization
+	var scope = 'user-read-private user-read-email playlist-read-private playlist-modify-private playlist-modify-public';
+	res.redirect( 'https://accounts.spotify.com/authorize?' +
+		querystring.stringify( {
+			response_type: 'code',
+			client_id: global.client.spotify.appId,
+			scope: scope,
+			redirect_uri: global.client.spotify.redirectUri,
+			state: state
+		} ) );
 };
